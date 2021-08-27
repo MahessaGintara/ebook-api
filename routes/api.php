02e67d83a3227bbbc4463afac8954f37d1981f89
MahessaGintaraDwiRosliyadi <3103119104@student.smktelkom-pwt.sch.id>
2021-08-27 22:12:02 +0700

    migration

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/me', [AuthController::class,'me']);
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::apiResource('/books', BookController::class);
